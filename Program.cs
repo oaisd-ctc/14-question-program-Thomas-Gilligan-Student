ed3e4f664bbe6747e4dad7b8284eacdec687830c
Thomas-Gilligan-Student <thgi236@student.oaisd.org>
2024-10-22 17:18:00 +0000

    Recreated 14 question program using modules

--- Program.cs
+++ Program.cs
@@ -1,62 +1,113 @@
 ﻿using System;
 
 class Program {
+
+    static String name;
+    static String age;
+    static String eyeColor;
+    static String hairColor;
+    static String shoeSize;
+    static String favoriteColor;
+    static String favoriteTVorMovie;
+    static String favoriteTeacher;
+    static String favoriteClass;
+    static String favoriteHoliday;
+    static String favoriteSeason;
+    static String dreamJob;
+    static String numberOfSiblings;
+    static int ageIn5Years;
+
     public static void Main(string[] args) {
 
-        Console.Clear();
+        AskName();
+        AskAge();
+        AskEyeColor();
+        AskHairColor();
+        AskShoeSize();
+        AskFavoriteColor();
+        AskFavoriteTvShow();
+        AskFavoriteTeacher();
+        AskFavoriteClass();
+        AskFavoriteHoliday();
+        AskFavoriteSeason();
+        AskDreamJob();
+        CalculateAgeIn5Years(age);
+        AskNumberOfSiblings();
+        DisplaySummaryMessage();
+
+    }
+
+    public static void AskName(){
         Console.WriteLine("What is your full name?");
-        string name = Console.ReadLine();
+        name = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskAge(){
         Console.WriteLine("How old are you?");
-        int age = int.Parse(Console.ReadLine());
+        age = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskEyeColor(){
         Console.WriteLine("What is your eye color");
-        string eyeColor = Console.ReadLine();
+        eyeColor = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskHairColor(){
         Console.WriteLine("What is your hair color?");
-        string hairColor = Console.ReadLine();
+        hairColor = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskShoeSize(){
         Console.WriteLine("What is your shoe size?");
-        float shoeSize = float.Parse(Console.ReadLine());
+        shoeSize = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskFavoriteColor(){
         Console.WriteLine("What is your favorite color?");
-        string favColor = Console.ReadLine();
+        favoriteColor = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskFavoriteTvShow(){
         Console.WriteLine("What is your favorite TV show/movie?");
-        string favFilm = Console.ReadLine();
+        favoriteTVorMovie = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskFavoriteTeacher(){
         Console.WriteLine("Who is your favorite teacher?");
-        string favTeacher = Console.ReadLine();
+        favoriteTeacher = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskFavoriteClass(){
         Console.WriteLine("What is your favorite class?");
-        string favClass = Console.ReadLine();
+        favoriteClass = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskFavoriteHoliday(){
         Console.WriteLine("What is your favorite holiday?");
-        string favHoliday = Console.ReadLine();
+        favoriteHoliday = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskFavoriteSeason(){
         Console.WriteLine("What is your favorite season?");
-        string favSeason = Console.ReadLine();
+        favoriteSeason = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void AskDreamJob(){
         Console.WriteLine("What is your dream job?");
-        string dreamJob = Console.ReadLine();
+        dreamJob = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
+    public static void CalculateAgeIn5Years(String age){
+        ageIn5Years = int.Parse(age) + 5;
+        
+    }
+
+    public static void AskNumberOfSiblings(){
         Console.WriteLine("How many siblings do you have?");
-        int numOfSiblings = int.Parse(Console.ReadLine());
+        numberOfSiblings = Console.ReadLine() ?? "?";
+    }
 
-        Console.Clear();
-        Console.WriteLine($"My friend's name is {name}. {name} is {age} years old. {name}'s eye color is {eyeColor} and their hair color is {hairColor}. {name}'s shoe size is {shoeSize}. {name}'s favourite color is {favColor}, favourite movie/TV show is {favFilm}, favourite teacher is {favTeacher}, favourite class is {favClass}, favourite holiday is {favHoliday}, and favourite season is {favSeason}. Their dream job is to be a(n) {dreamJob}. In 5 years, they will be {age + 5} years old. {name} has {numOfSiblings} siblings.");
-
+    public static void DisplaySummaryMessage(){
+        Console.WriteLine($"My friend's name is {name}. {name} is {age} years old. {name}'s eye color is {eyeColor} and their hair color is {hairColor}. {name}'s shoe size is {shoeSize}. {name}'s favourite color is {favoriteColor}, favourite movie/TV show is {favoriteTVorMovie}, favourite teacher is {favoriteTeacher}, favourite class is {favoriteClass}, favourite holiday is {favoriteHoliday}, and favourite season is {favoriteSeason}. Their dream job is to be a(n) {dreamJob}. In 5 years, they will be {age + 5} years old. {name} has {numberOfSiblings} siblings.");
     }
 }
